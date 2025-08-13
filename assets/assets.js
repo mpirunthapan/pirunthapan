f363f78eeb118ac8ea2d6be425ad9f7f700f28cb
mpirunthapan <mpirunthapan@gmail.com>
2025-08-13 22:16:52 +0530

    python commit

--- assets/assets.js
+++ assets/assets.js
@@ -140,10 +140,10 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js, Angular, NodeJS, PHP, C#, VB.net' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript, React Js, Next Js, Python, Django, NodeJS, PHP, C#, VB.net' },
     { icon: assets.db_icon, iconDark: assets.db_icon_dark, title: 'Databases', description: 'MSSQL, MySQL, PostgreSQL, MongoDB' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Sc in Computer Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 4 projects' }
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
 
 export const toolsData = [
