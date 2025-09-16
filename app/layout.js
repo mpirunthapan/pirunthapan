3a6084739c9b87bac367e26fec53f4aeed5d05f6
mpirunthapan <mpirunthapan@gmail.com>
2025-09-16 15:12:42 +0530

    google tag commit

--- app/layout.js
+++ app/layout.js
@@ -37,6 +37,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NDD7H4D7');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Google Analytics script */}
         <Script
           strategy="afterInteractive"
@@ -91,7 +108,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
-        <AnalyticsListener />
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NDD7H4D7"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
